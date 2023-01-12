2ecefb4bd4b5c2dd19eb132d957dbd634694513e
MadLies <jwilchesc@unal.edu.co>
2023-01-12 02:13:02 -0500

    Interactions whit the buttons

--- Hangman/Hangman/MainPage.xaml.cs
+++ Hangman/Hangman/MainPage.xaml.cs
@@ -6,6 +6,15 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
     #region  UI Properties
 
+    public string Message
+    {
+        get => message; 
+        set
+        {
+            message = value;
+            OnPropertyChanged();
+        }
+    }
     public string Spotlight
     {
         get => spotlight;
@@ -34,6 +43,7 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
     private string spotlight;
     List<char> guessed = new List<char>();
     private List<char> characters = new List<char>();
+    private string message;
     #endregion
     public MainPage()
     {
@@ -57,5 +67,38 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
         Spotlight = string.Join(" ", temp);
     }
     #endregion
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var btn = sender as Button;
+        if (btn != null)
+        {
+            var letter = btn.Text;
+            btn.IsEnabled = false;
+            HandleGuess(letter[0]);
+        }
+    }
+
+    private void HandleGuess(char letter)
+    {
+        if (guessed.IndexOf(letter) == -1)
+        {
+            guessed.Add(letter);
+        }
+        if (answer.IndexOf(letter) >= 0)
+        {
+            CalculateWord(answer, guessed);
+            CheckIfWon();
+        }
+
+    }
+
+    private void CheckIfWon()
+    {
+        if (Spotlight.Replace(" ", "") == answer)
+        {
+            Message = "You Win !!!";
+        }
+    }
 }
 
